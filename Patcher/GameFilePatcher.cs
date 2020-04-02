6b12948d4bfc10488dc4cabaecd951caa53a23e3
Zero is Black <49047211+shlifedev@users.noreply.github.com>
2020-04-02 16:43:53 +0900

    xml changer

--- Patcher/GameFilePatcher.cs
+++ Patcher/GameFilePatcher.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Patcher
 {
-    class Patcher
+    public class GameFilePatcher
     {
     }
 }
---
+++ Patcher/PatchText.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patcher
+{
+    public static class PatchText
+    {
+        public static string LANGUAGE_XML = @"
+<Languages DefaultLanguage=""English"">
+  <!--Maps English fonts to Foreign fonts to use on language change--> 
+  <!-- English -->
+  <Language id=""English"" DefaultFont=""Galahad"">
+    <Map From=""FiraSansExtraCondensed-Light"" To=""FiraSansExtraCondensed-Light""/>
+    <Map From=""FiraSansExtraCondensed-Medium"" To=""FiraSansExtraCondensed-Medium""/>
+    <Map From=""FiraSansExtraCondensed-Regular"" To=""FiraSansExtraCondensed-Regular""/>
+    <Map From=""Galahad"" To=""Galahad""/>
+    <Map From=""Galahad_Numbers_Bold"" To=""Galahad_Numbers_Bold""/>
+  </Language> 
+  <!-- Türkçe -->
+  <Language id=""Türkçe"" DefaultFont=""Galahad"">
+    <Map From=""FiraSansExtraCondensed-Light"" To=""FiraSansExtraCondensed-Light""/>
+    <Map From=""FiraSansExtraCondensed-Medium"" To=""FiraSansExtraCondensed-Medium""/>
+    <Map From=""FiraSansExtraCondensed-Regular"" To=""FiraSansExtraCondensed-Regular""/>
+    <Map From=""Galahad"" To=""Galahad""/>
+    <Map From=""Galahad_Numbers_Bold"" To=""Galahad_Numbers_Bold""/>
+  </Language> 
+  <!-- Chinese -->
+  <Language id=""繁體中文"" DefaultFont=""simkai"">
+    <Map From=""FiraSansExtraCondensed-Light"" To=""simkai""/>
+    <Map From=""FiraSansExtraCondensed-Medium"" To=""simkai""/>
+    <Map From=""FiraSansExtraCondensed-Regular"" To=""simkai""/>
+    <Map From=""Galahad"" To=""simkai""/>
+    <Map From=""Galahad_Numbers_Bold"" To=""simkai""/>
+  </Language>
+  <Language id=""简体中文"" DefaultFont=""simkai"">
+    <Map From=""FiraSansExtraCondensed-Light"" To=""simkai""/>
+    <Map From=""FiraSansExtraCondensed-Medium"" To=""simkai""/>
+    <Map From=""FiraSansExtraCondensed-Regular"" To=""simkai""/>
+    <Map From=""Galahad"" To=""simkai""/>
+    <Map From=""Galahad_Numbers_Bold"" To=""simkai""/>
+  </Language> 
+  <!-- Japanese -->
+  <Language id=""日本語"" DefaultFont=""simkai""/> 
+  <!-- Korean -->
+  <Language id=""한국어"" DefaultFont=""simkai"">
+     <Map From=""FiraSansExtraCondensed-Light"" To=""simkai""/>
+    <Map From=""FiraSansExtraCondensed-Medium"" To=""simkai""/>
+    <Map From=""FiraSansExtraCondensed-Regular"" To=""simkai""/>
+    <Map From=""Galahad"" To=""simkai""/>
+    <Map From=""Galahad_Numbers_Bold"" To=""simkai""/>
+   </Language> 
+  <!-- French -->
+  <Language id=""Français"" DefaultFont=""FiraSansExtraCondensed-Regular""/>
+</Languages> 
+    ";
+    }
+}
