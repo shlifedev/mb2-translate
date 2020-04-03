0770f45095274d6419e2dd40560a278232d532a5
Zero is Black <49047211+shlifedev@users.noreply.github.com>
2020-04-03 13:42:43 +0900

    example
    example

---
+++ MBKoreanFontInstallerConsole/DriveManager.cs
@@ -0,0 +1,42 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace MBKoreanFontInstallerConsole
+{
+    public static class DriveManager
+    {
+        static DriveService Service = null;
+        public static void Init(DriveService service)
+        {
+            Service = service;
+        }
+
+        public static IEnumerable<Google.Apis.Drive.v3.Data.File> GetFolderFiles(string folderID, string filterExtention = null)
+        {
+            var request = Service.Files.List();
+            request.Q = $"'{folderID}' in parents";
+
+            if (filterExtention != null)
+            { 
+                var executeResult = request.Execute().Files.Where(x=>x.FullFileExtension.Contains(filterExtention));
+                return executeResult;
+            }
+            else
+            {
+                var executeResult = request.Execute().Files;
+                return executeResult;
+            } 
+        }
+    }
+}
---
+++ MBKoreanFontInstallerConsole/Program.cs
@@ -0,0 +1,53 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using MBKoreanFontInstallerConsole;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GoogleDrive
+{
+    class Program
+    { 
+        static string[] Scopes = { DriveService.Scope.DriveReadonly };
+        static string ApplicationName = "Drive API .NET Quickstart"; 
+        static void Credential()
+        {
+            UserCredential credential;
+            using (var stream =
+                new FileStream("../../credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+            // Create Drive API service.
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+            DriveManager.Init(service); 
+        }
+        static void Main(string[] args)
+        {
+            Credential();
+            var fils = DriveManager.GetFolderFiles("folder id");
+        } 
+ 
+    }
+}
